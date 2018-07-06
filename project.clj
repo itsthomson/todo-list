2a80ef335a5ee1ed3758e83e8960c7689ba29987
Thomson Nguyen <thomson@cantab.net>
2018-07-05 23:54:32 -0700

    Add hiccup to make things prettier

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.4.0"]
-                 [compojure "1.3.4"]]
+                 [compojure "1.3.4"]
+                 [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.9.6" :exclusions [org.clojure/clojure]]]
   :main todo-list.core
   :profiles {:dev
--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -2,46 +2,45 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found]]))
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]
+            [hiccup.core :refer :all]
+            [hiccup.page :refer :all]))
 
 (defn welcome
   "A ring handler to process all requests sent to the webapp"
   [request]
-  {:status 200
-   :body "<h1>Hello, Clojure World!</h1>
-          <p>Just futzing around with Clojure. This should automatically 
-             update thanks to <b>wrap-reload</b>!</p>"
-   :headers {}})
+  (html [:h1 "Hello, Clojure World!"]
+        [:p "This app should auto-update thanks to wrap-reload, and now looks 
+             prettier with hiccup."]))
 
 (defn goodbye
   "Saying Goodbye"
   [request]
-  {:status 200
-   :body "<h1>Goodbyeeeeeeeeee</h1>
-          <p>Getting the hang of how routes work in Clojure.</p>"
-   :headers {}})
+  (html [:h1 "Goodbyeeeeeeee"]
+        [:p "Getting the hang of how routes and hiccup work in Clojure."]))
 
 (defn about
   "Information about the website developer"
   [request]
-  {:status 200
-   :body "<p>Making the most of my EIR time...</p>"
-   :headers {}})
+  (html5 {:lang "en"}
+         [:head (include-js "myscript.js") (include-css "mystyle.css")]
+         [:body
+           [:div [:h1 {:class "info"} "About Me"]]
+           [:div [:p "I'm making the most of my EIR time..."]]]))
 
 (defn request-info
   "View the information contained in the request, useful for debugging"
   [request]
-  {:status 200
-   :body (pr-str request)
-   :headers {}})
+  (html [:h1 "HTML Request"]
+        [:p (pr-str request)]))
 
 (defn hello
   "Personalized greeting demonstrating variable path elements"
   [request]
   (let [name (get-in request [:route-params :name])]
-    {:status 200
-     :body (str "Hello " name ". I got your name from the URL.")
-     :headers {}}))
+    (html [:h1 "Variable Path Example"]
+          [:p (str "Hello " name ". I got your name from the URL.")])))
 
 (def operands {"+" + "-" - "*" * ":" /})
 
@@ -54,13 +53,11 @@
         op (get-in request [:route-params :op])
         f (get operands op)]
      (if f
-        {:status 200
-         :body (str "Calculated result: " (f a b))
-         :headers {}}
-        {:status 404
-         :body "Sorry, unknown operator. I only recognize [+, -, *, :] (: is 
-                for division)."
-         :headers {}})))
+        (html [:h1 "Calculator Example"]
+              [:p (str "Calculated result: " (f a b))])
+        (html [:h1 "Calculator Example (Error)"]
+              [:p "Sorry, unknown operator. I only recognize [+, -, *, :] (: is 
+                   for division"]))))
 
 (defroutes app
   (GET "/" [] welcome)
