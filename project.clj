f0e953f614e7da0dcfcb29c2f328b161ef82c364
Thomson Nguyen <thomson@cantab.net>
2018-07-07 17:22:50 -0700

    Re-add Datomic, but exclude Jetty
    This was humbling--spent a long time figuring out why I was getting
    java.lang.NoClassDefFoundERror when running the webserver, and realized
    that this project depends on a more recent version of Jetty (through
    ring) than Datomic. Removing the version of Jetty used by Datomic (per
    [1]) fixes all this.
    
    [1] https://docs.datomic.com/cloud/troubleshooting.html#troubleshooting-clients

--- project.clj
+++ project.clj
@@ -6,7 +6,11 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [ring "1.6.1"]
                  [compojure "1.3.4"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [com.datomic/client-pro "0.8.17"
+                  :exclusions [org.eclipse.jetty/jetty-client
+                               org.eclipse.jetty/jetty-http
+                               org.eclipse.jetty/jetty-util]]]
   :plugins [[lein-ring "0.12.4" :exclusions [org.clojure/clojure]]]
   :main todo-list.core
   :profiles {:dev
