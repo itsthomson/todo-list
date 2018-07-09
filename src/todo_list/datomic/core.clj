060788fc06a955c46b63671929668b4c18855972
Thomson Nguyen <thomson@cantab.net>
2018-07-08 17:39:29 -0700

    Hard code local dev Datomic settings + add helper functions
    Going to just do this local, inmem until I figure out how this works.

---
+++ src/todo_list/datomic/core.clj
@@ -0,0 +1,24 @@
+(ns todo-list.datomic.core
+  "Functions for interacting with Datomic.
+
+   Schema is defined in todo-list.datomic.schema"
+  (:require [datomic.client.api :as d]
+            [todo-list.datomic.schema :as schema]))
+
+; In the Datomic directory locally:
+; bin/run -m datomic.peer-server -h localhost -p 8998 -a myaccesskey,mysecret\ 
+; -d hello,datomic:mem://todo-list
+
+(def dev-cfg {:server-type :peer-server
+              :access-key "myaccesskey"
+              :secret "mysecret"
+              :endpoint "localhost:8998"})
+
+(def client (d/client dev-cfg))
+
+(def conn (d/connect client {:db-name "todo-list"}))
+
+(defn transact-schema
+  "Transact the schema into Datomic"
+  [conn schema-map]
+  (d/transact conn schema-map))
--- src/todo_list/datomic/schema.clj
+++ src/todo_list/datomic/schema.clj
@@ -2,37 +2,31 @@
   "A Datomic schema for todo lists.
    Tasks have an id, name, description, boolean state (not done or done), 
    datetime last modified, and datetime created."
-  (:require [datomic.api :as d]))
+  (:require [datomic.client.api :as d]))
 
 (def schema-txn
   [{:db/doc "Task unique ID"
-    :db/id #db/id[:db.part/db]
     :db/ident :task/id
     :db/valueType :db.type/uuid
     :db/unique :db.unique/identity
     :db/cardinality :db.cardinality/one}
    {:db/doc "Task name"
-    :db/id #db/id[:db.part/db]
     :db/ident :task/name
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one}
    {:db/doc "Task description"
-    :db/id #db/id[:db.part/db]
     :db/ident :task/description
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one}
    {:db/doc "Task state"
-    :db/id #db/id[:db.part/db]
     :db/ident :task/state
     :db/valueType :db.type/boolean
     :db/cardinality :db.cardinality/one}
    {:db/doc "Task last modified"
-    :db/id #db/id[:db.part/db]
     :db/ident :task/modified
     :db/valueType :db.type/instant
     :db/cardinality :db.cardinality/one}
    {:db/doc "Task created"
-    :db/id #db/id[:db.part/db]
     :db/ident :task/created
     :db/valueType :db.type/instant
     :db/cardinality :db.cardinality/one}])
